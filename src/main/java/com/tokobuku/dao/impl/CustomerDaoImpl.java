d38e20627368bea584b1d9e9c48fa9c888590c92
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2018-09-28 06:10:33 +0700

    Contoh penerapan Spring JDBC

---
+++ src/main/java/com/tokobuku/dao/impl/CustomerDaoImpl.java
@@ -0,0 +1,84 @@
+package com.tokobuku.dao.impl;
+
+import com.tokobuku.dao.CustomerDao;
+import com.tokobuku.model.Customer;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CustomerDaoImpl implements CustomerDao {
+
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void insert(Customer customer) {
+        String sql = "INSERT INTO CUSTOMER " +
+                "(cust_id, full_name, address, email) " +
+                "VALUES " +
+                "(?, ?, ?, ?)";
+        Connection connection = null;
+
+        try {
+            connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, customer.getCustId());
+            preparedStatement.setString(2, customer.getFullName());
+            preparedStatement.setString(3, customer.getAddress());
+            preparedStatement.setString(4, customer.getEmail());
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public Customer findByCustomerId(int custId) {
+        String sql = "SELECT * FROM CUSTOMER " +
+                "WHERE " +
+                "cust_id = ?";
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, custId);
+            Customer customer = null;
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                customer = new Customer(
+                        resultSet.getInt("cust_id"),
+                        resultSet.getString("full_name"),
+                        resultSet.getString("address"),
+                        resultSet.getString("email")
+                );
+                resultSet.close();
+                preparedStatement.close();
+                return customer;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return null;
+    }
+}
